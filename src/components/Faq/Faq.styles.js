ed7189371664a152640bd901e9ca2aed13bfbbca
ajay-creator <ajayveeraveni119@gail.com>
2022-03-22 18:25:41 +0530

    added faqs

--- src/components/Faq/Faq.styles.js
+++ src/components/Faq/Faq.styles.js
@@ -5,8 +5,8 @@ export const FaqContainer = styled.div`
     padding:0;
     padding:0;
     width:100%;
-    font-family:arial;
-
+    font-family: 'Inter', sans-serif;
+    font-weight:400;
 `
 
 export const SideBar = styled.div`
@@ -17,31 +17,40 @@ export const SideBar = styled.div`
     padding: 0;
     width:30%;
     height:100vh;
-    background-color: #FBAB7E;
-    background-image: linear-gradient(62deg, #FBAB7E 0%, #F7CE68 100%);
+    /* background-color: #FBAB7E;
+    background-image: linear-gradient(62deg, #FBAB7E 0%, #F7CE68 100%); */
+    background: #16222A;  /* fallback for old browsers */
+    background: -webkit-linear-gradient(to right, #3A6073, #16222A);  /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(to right, #3A6073, #16222A); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 
+    font-family: 'Inter', sans-serif;
     padding:40px;
     display: flex;
     flex-direction:column;
     
     > h1{
-        margin-bottom:30px;
-        margin-top:60px;
-        font-weight:800;
+        margin-bottom:20px;
+        margin-top:80px;
+        font-weight:900;
         color:#ffffff;
         font-size:1.5rem;
+        @media screen and (max-width:600px){
+            margin-top:100px;
+        }
     }
 
     > a{
         text-decoration:none;
         margin:10px 0;
         color:rgba(256,256,256,0.8);
-        font-weight:500px;
-        font-family:arial;
+        font-weight:400;
 
         &:hover{
             font-weight:600;
             color:#ffffff;
+        }
+        @media screen and (max-width:600px){
+            border-bottom:2px solid #dddd;
         }
     }
 
@@ -49,7 +58,7 @@ export const SideBar = styled.div`
         width: 100%;
         padding:20px;
         position:relative ;
-        height: 400px;
+        height: 500px;
     }
 
 `
@@ -62,11 +71,10 @@ export const QueryContainer = styled.div`
     padding:50px;
 
     h1{
-        font-size:1.5rem;
+        font-size:2rem;
         margin-bottom:15px;
         padding-top:80px;
-        font-weight:800;
-        font-family:arial;
+        font-weight:900;
         
     }
     @media screen and (max-width:600px){
@@ -84,9 +92,14 @@ export const Query = styled.div`
     h2{
         font-weight:600;
         margin-bottom:10px;
-        font-size:1.25rem;
+        font-size:1.2rem;
+        font-weight:600;
     }
     p{
         margin-left:15px;
+        font-weight:400;
+    }
+    p a{
+        text-decoration:none;
     }
 `
