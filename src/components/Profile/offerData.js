a4448e8110ad70d6a2e6db8c63f9307f18845de5
Naman Tamrakar <namantam1@gmail.com>
2022-04-10 16:00:02 +0530

    Enable only proshow3

--- src/components/Profile/offerData.js
+++ src/components/Profile/offerData.js
@@ -1,53 +1,52 @@
 const offerData = [
   {
     name: "Registration",
-    tag: (
-      <p> Grab your ticket for spring spree 22. Register here </p>
-    ),
+    tag: <p> Grab your ticket for spring spree 22. Register here </p>,
     registration_fee: 300,
     key: "entry",
     slug: "paidForEvent",
   },
-  {
-    name: "Accomodation & Food",
-    tag: (
-      <p>
-        Book your stay for three days at NITW campus. Food will be provided for three days
-      </p>
-    ),
-    registration_fee: 900,
-    key: "accomodation",
-    slug: "paidForAccomodation",
-  },
-  {
-    name: "Pro-show 1",
-    tag: (
-      <p>
-        Ever wanted to experience the real Tillu anna behind the DJ Tillu song?
-        Wait no more, since <strong> Ram Miriyala </strong> is here to fulfil
-        your wish along with Indian Idol winner{" "}
-        <strong> Sriram Chandra </strong> . What are you waiting for? Hurry up
-        and grab your tickets now!
-      </p>
-    ),
-    registration_fee: 350,
-    key: "ps1",
-    slug: "paidForProshow1",
-  },
-  {
-    name: "Pro-show 2",
-    tag: (
-      <p>
-        {" "}
-        Go full on party mode today as you witness the DJ and Rock n Roll with{" "}
-        <strong> Underground Authority </strong> and{" "}
-        <strong> Sunburn DJ </strong>.Be the early bird today!{" "}
-      </p>
-    ),
-    registration_fee: 550,
-    key: "ps2",
-    slug: "paidForProshow2",
-  },
+  // {
+  //   name: "Accomodation & Food",
+  //   tag: (
+  //     <p>
+  //       Book your stay for three days at NITW campus. Food will be provided for
+  //       three days
+  //     </p>
+  //   ),
+  //   registration_fee: 900,
+  //   key: "accomodation",
+  //   slug: "paidForAccomodation",
+  // },
+  // {
+  //   name: "Pro-show 1",
+  //   tag: (
+  //     <p>
+  //       Ever wanted to experience the real Tillu anna behind the DJ Tillu song?
+  //       Wait no more, since <strong> Ram Miriyala </strong> is here to fulfil
+  //       your wish along with Indian Idol winner{" "}
+  //       <strong> Sriram Chandra </strong> . What are you waiting for? Hurry up
+  //       and grab your tickets now!
+  //     </p>
+  //   ),
+  //   registration_fee: 350,
+  //   key: "ps1",
+  //   slug: "paidForProshow1",
+  // },
+  // {
+  //   name: "Pro-show 2",
+  //   tag: (
+  //     <p>
+  //       {" "}
+  //       Go full on party mode today as you witness the DJ and Rock n Roll with{" "}
+  //       <strong> Underground Authority </strong> and{" "}
+  //       <strong> Sunburn DJ </strong>.Be the early bird today!{" "}
+  //     </p>
+  //   ),
+  //   registration_fee: 550,
+  //   key: "ps2",
+  //   slug: "paidForProshow2",
+  // },
   {
     name: "Pro-show 3",
     tag: (
@@ -57,8 +56,9 @@ const offerData = [
         <em>'Kabhii tumhhe'</em>, the romantic{" "}
         <em> 'Ek ladki ko dekha toh aisa laga' </em> and many more songs that
         leave us vibing. <br />
-        SpringSpree'22 brings to you the voice behind these hits, <strong>  Darshan Raval </strong>,
-        here at NIT Warangal on 10th of April. <br />
+        SpringSpree'22 brings to you the voice behind these hits,{" "}
+        <strong> Darshan Raval </strong>, here at NIT Warangal on 10th of April.{" "}
+        <br />
         This Spree, get ready to shake your <em> 'Kamariya' </em> and dance to
         the mesmerizing beats of <em> 'Chogada' </em>.{" "}
       </p>
